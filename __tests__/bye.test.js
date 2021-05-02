dd86ad8e97f7e1e14df451c6dc7be7eb59a6b52f
Michio Nakagawa <michio.nakagawa@gmail.com>
2021-05-03 00:51:35 +0900

    feat: first feature
    BREAKING CHANGE: it breaks something

--- __tests__/bye.test.js
+++
@@ -1,7 +0,0 @@
-const bye = require('@/bye')
-
-describe('bye', () => {
-  it('${name} to equal Bye, ${name}', () => {
-    expect(bye('taro')).toBe('Bye, taro')
-  })
-})
--- __tests__/hello.test.js
+++ __tests__/hello.test.js
@@ -1,7 +1,15 @@
-const hello = require('@/hello')
+const Hello = require('@/hello')
+let actual
+
+beforeEach(() => {
+  actual = new Hello('taro')
+});
 
 describe('hello', () => {
-  it('${name} to equal Hell, ${name}', () => {
-    expect(hello('taro')).toBe('Hello, taro')
+  it('${name} to equal Hello, ${name}', () => {
+    expect(actual.hello()).toBe('Hello, taro')
+  })
+  it('${name} to equal Bye, ${name}', () => {
+    expect(actual.bye()).toBe('Bye, taro')
   })
 })
