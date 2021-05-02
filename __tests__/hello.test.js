60253d255da10c8c64ce569251eefc69ba0ca2ad
Michio Nakagawa <michio.nakagawa@gmail.com>
2021-05-02 18:05:37 +0900

    fix: typo

--- __tests__/hello.test.js
+++ __tests__/hello.test.js
@@ -1,7 +1,7 @@
 const hello = require('@/hello')
 
 describe('hello', () => {
-  it('${name} to equal Hell, ${name}', () => {
+  it('${name} to equal Hello, ${name}', () => {
     expect(hello('taro')).toBe('Hello, taro')
   })
 })
